bda45b80a6799307d4ee4578902cac6a1c7810c0
cho2 <chso1324@naver.com>
2022-11-16 23:29:13 +0900

    feat: 볼, 스트라이크 개수 판단 기능

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -5,14 +5,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Controller {
     private int GO_STOP = 1;
-    private Integer[] userInput;
+    private List<Integer> userInput;
     private List<Integer> computerInput;
 
     public void run() {
@@ -30,6 +27,8 @@ public class Controller {
     public void start(){
         userInput = StartView.startGame();
         computerInput = makeComputerNums();
+        System.out.println(computerInput);
+        System.out.println(countBaseBall(userInput, computerInput));
 
     }
 
@@ -37,10 +36,10 @@ public class Controller {
 
     }
 
-    public static Integer[] checkInput(BufferedReader in){
+    public static List<Integer> checkInput(BufferedReader in){
         String input;
         char[] charIn;
-        Integer[] intIn;
+        List<Integer> intIn;
 
         try{
             input = in.readLine();
@@ -49,8 +48,10 @@ public class Controller {
         }
 
         charIn = input.toCharArray();
-        intIn = new Integer[input.length()];
-        Arrays.setAll(intIn, i -> Character.getNumericValue(charIn[i]));
+        intIn = new ArrayList<>(input.length());
+        for(int i = 0; i < charIn.length; i ++){
+            intIn.add(Character.getNumericValue(charIn[i]));
+        }
 
         // input이 3개 이상 아닐시
         if(charIn.length != 3){
@@ -65,7 +66,7 @@ public class Controller {
         }
         
         // 중복수
-        Set<Integer> set = new HashSet<Integer>(Arrays.asList(intIn));
+        Set<Integer> set = new HashSet<>(intIn);
         if(set.size() != 3){
             throw new IllegalArgumentException("Invalid Input: 중복되지 않는 3개 이상의 숫자를 입력입니다.");
         }
@@ -74,8 +75,29 @@ public class Controller {
 
     }
 
+    // Computer 랜덤 수 생성
     public static List<Integer> makeComputerNums(){
         return Randoms.pickUniqueNumbersInRange(1, 9, 3);
     }
+
+    // 볼, 스트라이크 개수 판단
+    public static List<Integer> countBaseBall(List<Integer> user, List<Integer> computer){
+        List<Integer> counts = Arrays.asList(0, 0);
+        Set<Integer> set = new HashSet<>(computer);
+        for(int i = 0; i < user.size(); i ++){
+            if(user.get(i) == computer.get(i)){
+                counts.set(0, counts.get(0) + 1);
+                continue;
+            }
+            if(set.contains(user.get(i))){
+                counts.set(1, counts.get(1) + 1);
+                continue;
+            }
+        }
+
+        return counts;
+    };
+
+
 
 }
--- src/main/java/baseball/view/StartView.java
+++ src/main/java/baseball/view/StartView.java
@@ -6,12 +6,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.List;
 
 public class StartView {
 
     private static BufferedReader in;
 
-    public static Integer[] startGame(){
+    public static List<Integer> startGame(){
         System.out.print("숫자를 입력해주세요 : ");
         in = new BufferedReader(new InputStreamReader(System.in));
         return Controller.checkInput(in);
